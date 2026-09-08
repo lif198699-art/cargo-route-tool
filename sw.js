29b69ab2965ad41b245e4ea69126896936d454bb
lif198699-art <lif198699@gmail.com>
2026-09-08 17:00:36 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'cargo-route-v1';
+var APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(key) {
+        return key !== CACHE_NAME ? caches.delete(key) : Promise.resolve();
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
